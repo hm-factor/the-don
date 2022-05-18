a960faf247de9513b066f6e53301e6ffd3aa6823
hm-factor <hsmanel@gmail.com>
2022-05-18 13:00:18 -0400

    update homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,16 +3,12 @@ import Layout from '../components/layout'
 
 import '../styles/global.css'
 
-const IndexPage = () => {
+export default function HomePage() {
   return (
-    <main>
       <Layout>
         <div>
           the don
         </div>
       </Layout>
-    </main>
   )
 }
-
-export default IndexPage
