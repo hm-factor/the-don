d50e5bee37d9a1217264d5693b36cd18eb67d9c2
hm-factor <hsmanel@gmail.com>
2022-05-18 12:57:37 -0400

    basic routing setup for three navbar pages

---
+++ src/pages/music.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function MusicPage() {
+
+    return (
+        <Layout>
+            Music
+        </Layout>
+    )
+}
